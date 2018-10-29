3406d83fbc02c787edc02c9a9e2b41a7bce3bf9c
GildardoJ <gorozco@csumb.edu>
2018-10-28 20:55:38 -0700

    items list and item details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsComponent } from './items/items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PostsComponent,
     UsersComponent,
     DetailsComponent,
+    ItemsComponent,
+    ItemDetailsComponent,
   ],
   imports: [
     BrowserModule,
